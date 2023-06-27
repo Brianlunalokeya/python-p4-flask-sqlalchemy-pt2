c4f9afc69337a6a2abca0922eeef7705cd26e18a
+ <+>
2023-06-27 05:16:47 +0300

    Done with Assignment

--- server/models.py
+++ server/models.py
@@ -4,7 +4,6 @@ from sqlalchemy import MetaData
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
-
 db = SQLAlchemy(metadata=metadata)
 
 class Owner(db.Model):
